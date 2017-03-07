e301f1f4ba121c637f2c491c311e102570b77fb6
Katelyn Smith <Katelyn Smith>
2017-03-06 22:54:34 -0600

    Initial commit

---
+++ app/src/main/java/com/example/towersofhanoi/MainActivity.java
@@ -0,0 +1,107 @@
+
+package com.example.towersofhanoi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ViewGroup stagingArea; //represents area where popped disk waits for destination
+    private ViewGroup tower0; //vertical linear layout - represents far left tower
+    private ViewGroup tower1; //vertical linear layout - represents middle tower
+    private ViewGroup tower2; //vertical linear layout - represents far right tower
+    private String currMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
+        this.stagingArea = (ViewGroup)this.findViewById(R.id.stagingArea);
+        this.tower0 = (ViewGroup)this.findViewById(R.id.tower0);
+        this.tower1 = (ViewGroup)this.findViewById(R.id.tower1);
+        this.tower2 = (ViewGroup)this.findViewById(R.id.tower2);
+        this.currMode = "SRC";
+    }
+
+    public void tower0ButtonPressed(View v) {
+        if (this.currMode == "SRC") ;
+        {
+            if (this.tower0.getChildCount() > 0) {
+
+                {
+                    View temp = this.tower0.getChildAt(0);
+                    this.tower0.removeViewAt(0);
+                    this.stagingArea.addView(temp);
+                    this.currMode = "DEST";
+                }
+            } else if (this.currMode == "DEST") {
+                View temp = this.stagingArea.getChildAt(0);
+                this.stagingArea.removeViewAt(0);
+                this.tower0.addView(temp, 0);
+                this.currMode = "SRC";
+            }
+
+        }
+        if(this.tower0.getChildCount() > 1 || this.tower0.getChildCount() > 2)
+        {
+            this.tower0.removeView(this.tower1);
+            this.tower0.removeView(this.tower2);
+            this.tower0.addView(this.tower0);
+        }
+
+
+
+    }
+
+    public void tower1ButtonPressed(View v)
+    {
+        if (this.currMode == "SRC");
+        {
+        if(this.tower1.getChildCount() > 0)
+        {
+            {
+                View temp = this.tower1.getChildAt(0);
+                this.tower1.removeViewAt(0);
+                this.stagingArea.addView(temp);
+                this.currMode = "DEST";
+            }
+        }
+            else
+            {
+                View temp = this.stagingArea.getChildAt(0);
+                this.stagingArea.removeViewAt(0);
+                this.tower1.addView(temp,0);
+                this.currMode = "SRC";
+            }
+        }
+        if(this.tower1.getChildCount() > 2)
+        {
+            this.tower1.removeView(this.tower2);
+            this.tower1.addView(this.tower1);
+        }
+    }
+    public void tower2ButtonPressed(View v)
+    {
+        if(this.currMode == "SRC")
+        {
+            if (this.tower2.getChildCount() > 0)
+            {
+                View temp = this.tower2.getChildAt(0);
+                this.tower2.removeViewAt(0);
+                this.stagingArea.addView(temp);
+                this.currMode = "DEST";
+            }
+        }
+            else
+            {
+                View temp = this.stagingArea.getChildAt(0);
+                this.stagingArea.removeViewAt(0);
+                this.tower2.addView(temp,0);
+                this.currMode = "SRC";
+            }
+        }
+    }
